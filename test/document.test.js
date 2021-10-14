7d97f63a419d5cf44ff2ddc779b166ebc3eb056d
Rebecca Axelsson <ra223ai@student.lnu.se>
2021-10-14 11:29:54 +0200

    Add tests for length

--- test/document.test.js
+++ test/document.test.js
@@ -2,30 +2,50 @@ import { wordAndDotGrammar } from './testData.js'
 import { initTokenizer } from '../src/tokenizer/main.js'
 import { Document } from '../src/Document.js'
 
-describe('Get sentences', () => {
-  it('All sentences', () => {
+describe('Get sentences in string format', () => {
+  it('All sentences returns expected strings', () => {
     const tokenizer = initTokenizer(wordAndDotGrammar, 'Aa bb. A b!')
     const myDocument = new Document(tokenizer)
     const sentences = myDocument.getAllSentences()
     expect(sentences).toEqual(['Aa bb.', 'A b!'])
   })
-  it('Regular sentence', () => {
+  it('Regular sentence returns expected string', () => {
     const tokenizer = initTokenizer(wordAndDotGrammar, 'A b.')
     const myDocument = new Document(tokenizer)
     const regularSentences = myDocument.getRegularSentences()
-    // const sentence = regularSentences[0]
     expect(regularSentences[0]).toBe('A b.')
   })
-  it('Expressions', () => {
+  it('Expressions returns expected string', () => {
     const tokenizer = initTokenizer(wordAndDotGrammar, 'Aa bb. A b!')
     const myDocument = new Document(tokenizer)
     const expressions = myDocument.getExpressions()
     expect(expressions[0]).toBe('A b!')
   })
-  it('Questions', () => {
+  it('Questions returns expected string', () => {
     const tokenizer = initTokenizer(wordAndDotGrammar, 'Aa bb. A b? Bb!')
     const myDocument = new Document(tokenizer)
     const questions = myDocument.getQuestions()
     expect(questions[0]).toBe('A b?')
+  })
+})
+
+describe('Get the expected length', () => {
+  it('All sentences should be correct amount if one type of sentence', () => {
+    const tokenizer = initTokenizer(wordAndDotGrammar, 'Aa bb. A b.')
+    const myDocument = new Document(tokenizer)
+    const sentences = myDocument.getAllSentences()
+    expect(sentences.length).toBe(2)
+  })
+  it('All sentences should be correct amount if two types of sentences', () => {
+    const tokenizer = initTokenizer(wordAndDotGrammar, 'Aa bb. A b!')
+    const myDocument = new Document(tokenizer)
+    const sentences = myDocument.getAllSentences()
+    expect(sentences.length).toBe(2)
+  })
+  it('All sentences shpuld be correct amount if three types of sentences', () => {
+    const tokenizer = initTokenizer(wordAndDotGrammar, 'Aa bb. A b? Bb!')
+    const myDocument = new Document(tokenizer)
+    const sentences = myDocument.getAllSentences()
+    expect(sentences.length).toBe(3)
   })
 })
