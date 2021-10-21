1ecd47df5ceb817e83fd79fb0ebe3bbf773751cb
Rebecca Axelsson <ra223ai@student.lnu.se>
2021-10-21 20:56:00 +0200

    ✅  Update tests for words

--- test/document.test.js
+++ test/document.test.js
@@ -6,27 +6,25 @@ import { parse } from '../src/app.js'
 import { InvalidSentenceFormat } from '../src/errors/InvalidSentenceFormat.js'
 
 describe('Get sentences in string format', () => {
-  it('All sentences returns expected strings', () => {
+  it('TC1. All sentences returns expected strings', () => {
     const myDocument = parse('Aa bb. A b!')
     const sentences = myDocument.fetchAllSentences()
-    // const stringSentences = []
-    // sentences.parsedSentences.forEach(s => stringSentences.push(s.getStringSentence))
     const stringSentences = sentences.parsedSentences.map(sentence => sentence.getStringSentence)
     expect(stringSentences).toEqual(['Aa bb.', 'A b!'])
   })
-  it('Regular sentence returns expected string', () => {
+  it('TC2. Regular sentence returns expected string', () => {
     const myDocument = parse('A b.')
     const sentences = myDocument.fetchRegularSentences()
     const stringSentences = sentences.map(sentence => sentence.getStringSentence)
     expect(stringSentences[0]).toBe('A b.')
   })
-  it('Expressions returns expected string', () => {
+  it('TC3. Expressions returns expected string', () => {
     const myDocument = parse('Aa bb. A b!')
     const sentences = myDocument.fetchExpressions()
     const stringSentences = sentences.map(sentence => sentence.getStringSentence)
     expect(stringSentences[0]).toBe('A b!')
   })
-  it('Questions returns expected string', () => {
+  it('TC4. Questions returns expected string', () => {
     const myDocument = parse('Aa bb. A b? Bb!')
     const sentences = myDocument.fetchQuestions()
     const stringSentences = sentences.map(sentence => sentence.getStringSentence)
@@ -35,17 +33,17 @@ describe('Get sentences in string format', () => {
 })
 
 describe('Get the expected length', () => {
-  it('All sentences should be correct amount if one type of sentence', () => {
+  it('TC5. All sentences should be correct amount if one type of sentence', () => {
     const myDocument = parse('Aa bb. A b.')
     const sentences = myDocument.fetchAllSentences()
     expect(sentences.parsedSentences.length).toBe(2)
   })
-  it('All sentences should be correct amount if two types of sentences', () => {
+  it('TC6. All sentences should be correct amount if two types of sentences', () => {
     const myDocument = parse('Aa bb. A b!')
     const sentences = myDocument.fetchAllSentences()
     expect(sentences.parsedSentences.length).toBe(2)
   })
-  it('All sentences should be correct amount if three types of sentences', () => {
+  it('TC7. All sentences should be correct amount if three types of sentences', () => {
     const myDocument = parse('Aa bb. A b? Bb!')
     const sentences = myDocument.fetchAllSentences()
     expect(sentences.parsedSentences.length).toBe(3)
@@ -53,32 +51,32 @@ describe('Get the expected length', () => {
 })
 
 describe('Error handling', () => {
-  it('Should throw error if sentence has no valid end token.', () => {
+  it('TC8. Should throw error if sentence has no valid end token.', () => {
     expect(() => parse('Bb')).toThrow(InvalidEndTokenError)
   })
-  it('Should throw error if unvalid tokens in text string.', () => {
+  it('TC9. Should throw error if unvalid tokens in text string.', () => {
     expect(() => parse('Bb*')).toThrow(Error)
   })
-  it('Should throw error if string is empty.', () => {
+  it('TC10. Should throw error if string is empty.', () => {
     expect(() => parse('  ')).toThrow(Error)
   })
-  it('Should throw error if sentence has two end tokens.', () => {
+  it('TC11. Should throw error if sentence has two end tokens.', () => {
     expect(() => parse('Aa!?')).toThrow(InvalidSentenceFormat)
   })
 })
 
 describe('Types of sentences', () => {
-  it('Sentence should be of type regular sentence.', () => {
+  it('TC12. Sentence should be of type regular sentence.', () => {
     const myDocument = parse('A b.')
     const sentence = myDocument.fetchAllSentences().parsedSentences[0]
     expect(sentence instanceof RegularSentence).toEqual(true)
   })
-  it('Sentence should be of type expression.', () => {
+  it('TC13. Sentence should be of type expression.', () => {
     const myDocument = parse('A b!')
     const sentence = myDocument.fetchAllSentences().parsedSentences[0]
     expect(sentence instanceof Expression).toEqual(true)
   })
-  it('Sentence should be of type question.', () => {
+  it('TC14. Sentence should be of type question.', () => {
     const myDocument = parse('A b?')
     const sentence = myDocument.fetchAllSentences().parsedSentences[0]
     expect(sentence instanceof Question).toEqual(true)
@@ -86,18 +84,20 @@ describe('Types of sentences', () => {
 })
 
 describe('Words', () => {
-  it('Sentence should have two words.', () => {
+  it('TC15. Sentence should have two words.', () => {
     const myDocument = parse('A b.')
     const sentences = myDocument.fetchAllSentences()
     const sentence = sentences.parsedSentences[0]
-    const words = sentence._tokens
-    expect(words.length).toEqual(3)
+    const tokens = sentence._tokens
+    const words = tokens.filter(token => token.tokenType === 'WORD')
+    expect(words.length).toEqual(2)
   })
-  it('Second word should be \'b\'.', () => {
+  it('TC16. Second word should be \'b\'.', () => {
     const myDocument = parse('A b.')
     const sentences = myDocument.fetchAllSentences()
     const sentence = sentences.parsedSentences[0]
-    const words = sentence._tokens
+    const tokens = sentence._tokens
+    const words = tokens.filter(token => token.tokenType === 'WORD')
     expect(words[1].tokenValue).toEqual('b')
   } )
 })
